06c2c588cb235c84f514ef39cabd5df0b69869a4
Johan Ljungdahl <johan.ljungdahl@yahoo.se>
2020-06-06 20:51:10 +0200

    Initial commit

---
+++ src/app/app.common.ts
@@ -0,0 +1,9 @@
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
+
+export const componentDeclarations: any[] = [
+  ItemsComponent,
+  ItemDetailComponent
+];
+
+export const providerDeclarations: any[] = [];
